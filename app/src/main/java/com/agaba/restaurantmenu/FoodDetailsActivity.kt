11a87d5c3ecf41085dbee2bb86b7db661858a217
abelagaba <77072924+abelagaba@users.noreply.github.com>
2022-06-27 13:38:34 +0300

    initial commit

---
+++ app/src/main/java/com/agaba/restaurantmenu/FoodDetailsActivity.kt
@@ -0,0 +1,11 @@
+package com.agaba.restaurantmenu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FoodDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_food_details)
+    }
+}
